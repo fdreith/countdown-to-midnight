8d97ef745e1620676436afd680fbedcd3bea7356
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2019-12-10 16:10:28 -0800

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    number = 10
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number += -1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(time)
+    time = 5
+    until time <= 0
+        sleep (1) 
+        time += -1
+    end
+end
+
+# sleep 2
+# That'll sleep for 2 seconds.
